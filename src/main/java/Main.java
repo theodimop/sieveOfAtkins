ec560630ed7437a298a443c8ec7e3c74103782dc
thodwrisDimopoulos <dj_diteo@hotmail.com>
2017-10-14 23:16:07 +0100

    Finished updated!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,56 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Created by dj_di_000 on 14/10/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        int N;
-        if (args.length < 1) {
-            if ((N = Integer.getInteger(args[0], -1)) < 0) {
+        int N = -1;
+
+        //argument and integrity constraints check
+        try {
+            if (args.length < 1 || (N = Integer.parseInt(args[0])) < 0) {
                 printUsage();
+                System.exit(0);
             }
+
+            List<Integer> primes = PrimeFactory.findPrimes(N);
+            printPrimeMultiplicationTable(primes);
+//            System.out.println(primes.size());
+
+        } catch (NumberFormatException e) {
+            printUsage();
+            System.exit(0);
+
+        } catch (OutOfMemoryError e) {
+            int MB = 1024 * 1024;
+            Runtime runtime = Runtime.getRuntime();
+            System.err.println("Ooops! You need more memory to get the " + N + " first primes.");
+            System.err.println("You can allocate more memory by executing with option e.g: -Xmx256m ");
+            System.err.println("Currently you allocate " + runtime.totalMemory() / MB + " MB. Try to increase this" +
+                    "number to " + 2 * runtime.maxMemory() / MB + " MB.");
         }
 
+    }
 
+    private static void printPrimeMultiplicationTable(List<Integer> primes) {
+        int format = String.valueOf(Math.pow(primes.get(primes.size() - 1), 2)).length() - 1;
+
+        System.out.format("%s%" + format + "s%s", "|", " ", "|");
+        IntStream.range(0, primes.size()).forEach(i -> System.out.format("%" + format + "d%s", primes.get(i), "|"));
+        System.out.println();
+        IntStream.range(0, primes.size()).forEach(i -> {
+            System.out.format("%s%" + format + "d", "|", primes.get(i));
+            primes.forEach(p -> System.out.format("%s%" + format + "d", "|", primes.get(i) * p));
+            System.out.format("%s", "|\n");
+        });
     }
 
     private static void printUsage() {
         System.err.println("Usage: program parameter1. \n" +
                 "Parameter1 should be a positive integer.");
     }
+
+
 }
--- src/main/java/PrimeFactory.java
+++ src/main/java/PrimeFactory.java
@@ -1,5 +1,89 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
 /**
  * Created by dj_di_000 on 14/10/2017.
  */
 public class PrimeFactory {
+    private int N;
+
+
+    /**
+     * Find primes with Sieve of Atkins algorithm.
+     * Complexity of (N / (log log N))
+     *
+     * @param
+     * @return List with primes inside [0,arrayLength]
+     */
+    public static List<Integer> findPrimes(int numberOfPrimes) {
+        int arrayLength = getNumberInterval(numberOfPrimes);
+        boolean[] isPrime = new boolean[arrayLength + 1];
+        double sqrt = Math.sqrt(arrayLength);
+
+        for (int x = 1; x <= sqrt; x++) {
+            for (int y = 1; y <= sqrt; y++) {
+                int n = 4 * x * x + y * y;
+                if (n <= arrayLength && (n % 12 == 1 || n % 12 == 5)) {
+                    isPrime[n] = !isPrime[n];
+                }
+
+                n = 3 * x * x + y * y;
+                if (n <= arrayLength && (n % 12 == 7)) {
+                    isPrime[n] = !isPrime[n];
+                }
+
+                n = 3 * x * x - y * y;
+                if (x > y && (n <= arrayLength) && (n % 12 == 11)) {
+                    isPrime[n] = !isPrime[n];
+                }
+            }
+        }
+        for (int n = 5; n <= sqrt; n++) {
+            if (isPrime[n]) {
+                int s = n * n;
+                for (int k = s; k <= arrayLength; k += s) {
+                    isPrime[k] = false;
+                }
+            }
+        }
+
+        List<Integer> primes = new ArrayList<>();
+
+        if (arrayLength > 2)
+            primes.add(2);
+
+
+        if (arrayLength > 3)
+            primes.add(3);
+
+        //Java 8 streams are 1000ms slower (no step)  for arrayLength = 2^28
+        //Java 9 supports step but keep java8 for compatibility
+        int n = 5;
+        do {
+            if (isPrime[n])
+                primes.add(n);
+            n += 2;
+        } while (n <= arrayLength && primes.size() < numberOfPrimes);
+
+        return primes;
+    }
+
+    public static int getNumberInterval(int n) {
+        if (n < 168) {
+            return 1000;
+        } else if (n < 1229) {
+            return 10000;
+        } else if (n < 9592) {
+            return 100000;
+        } else if (n < 78498) {
+            return 1000000;
+        } else if (n < 664579) {
+            return 10000000;
+        } else if (n < 5761455)
+            return 100000000;
+        else
+            return 1000000000;
+
+    }
 }
+
